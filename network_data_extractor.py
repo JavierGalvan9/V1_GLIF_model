37c58fc819f2e093d9263265c2d4698925f600c5
Javier Galvan <jgalvan@ifisc.uib-csic.es>
2023-02-02 19:06:01 +0100

    First version of network_data_extractor

---
+++ network_data_extractor.py
@@ -0,0 +1,46 @@
+import os
+import pickle as pkl
+
+import h5py
+import numpy as np
+import pandas as pd
+
+data_dir = "GLIF_network/network"
+new_network = {"nodes": [], "edges": []}
+
+nodes_h5_path = os.path.join(data_dir, "v1_nodes.h5")
+nodes_h5_file = h5py.File(nodes_h5_path, "r")
+v1_node_ids = np.array(nodes_h5_file["nodes"]["v1"]["node_id"])
+v1_node_type_ids = np.array(nodes_h5_file["nodes"]["v1"]["node_type_id"])
+nodes_df = pd.read_csv(os.path.join(data_dir, "v1_node_types.csv"), delimiter=" ")
+for node_type_id in nodes_df["node_type_id"]:
+    mask = v1_node_type_ids == node_type_id
+    new_pop_dict = {"ids": v1_node_ids[mask]}
+    new_network["nodes"].append(new_pop_dict)
+
+edges_h5_path = os.path.join(data_dir, "v1_v1_edges.h5")
+edges_h5_file = h5py.File(edges_h5_path, "r")
+edge_type_ids = np.array(edges_h5_file["edges"]["v1_to_v1"]["edge_type_id"])
+source_node_ids = np.array(edges_h5_file["edges"]["v1_to_v1"]["source_node_id"])
+target_node_ids = np.array(edges_h5_file["edges"]["v1_to_v1"]["target_node_id"])
+syn_weights = np.array(edges_h5_file["edges"]["v1_to_v1"]["0"]["syn_weight"])
+edges_df = pd.read_csv("GLIF_network/network/v1_v1_edge_types.csv", delimiter=" ")
+
+for idx, (edge_type_id, edge_model, edge_delay) in edges_df[
+    ["edge_type_id", "model_template", "delay"]
+].iterrows():
+    mask = edge_type_ids == edge_type_id
+    new_pop_dict = {
+        "source": source_node_ids[mask],
+        "target": target_node_ids[mask],
+        "params": {
+            "model": edge_model,
+            #    'receptor_type':edge_receptor_type,
+            "delay": edge_delay,
+            "weight": syn_weights[mask],
+        },
+    }
+    new_network["edges"].append(new_pop_dict)
+
+with open(os.path.join(data_dir, "new_network_dat.pkl"), "wb") as file:
+    pkl.dump(new_network, file)
