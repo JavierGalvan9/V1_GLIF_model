fixing the loss masking, change in OS loss function

multi_training.py
@@ -178,25 +178,31 @@ def main(_):
             target_firing_rates = pkl.load(f) # they are in Hz and divided by 1000 to make it in kHz and match the dt = 1 ms
 
         tuning_angles = tf.constant(network['tuning_angle'], dtype=dtype)
-        for i, (key, value) in enumerate(target_firing_rates.items()):
-            # identify tne ids that are included in value["ids"]
-            neuron_ids = np.where(np.isin(network["node_type_ids"], value["ids"]))[0]
-            neuron_ids = tf.cast(neuron_ids, dtype=tf.int32)
-            target_firing_rates[key]['neuron_ids'] = neuron_ids
-            type_n_neurons = len(neuron_ids)
-            sorted_target_rates = losses.sample_firing_rates(value["rates"], type_n_neurons, flags.seed)
-            target_firing_rates[key]['sorted_target_rates'] = tf.cast(sorted_target_rates, dtype=tf.float32) 
-
         
         ### BUILD THE LOSS FUNCTIONS ###
         # Create rate and voltage regularizers
         delays = [int(a) for a in flags.delays.split(',') if a != '']
         if flags.core_loss:
             core_radius = 200
-            core_mask = other_v1_utils.isolate_core_neurons(network, radius=core_radius, data_dir=flags.data_dir)
-            core_mask = tf.constant(core_mask, dtype=tf.bool)
+            core_mask_np = other_v1_utils.isolate_core_neurons(network, radius=core_radius, data_dir=flags.data_dir)
+            core_mask = tf.constant(core_mask_np, dtype=tf.bool)
         else:
+            core_mask_np = None
             core_mask = None
+
+        # Sort the target firing rates for each neuron type
+        for i, (key, value) in enumerate(target_firing_rates.items()):
+            # identify tne ids that are included in value["ids"]
+            neuron_ids = np.where(np.isin(network["node_type_ids"], value["ids"]))[0]
+            if core_mask_np is not None:
+                # if core_mask is not None, use only neurons in the core
+                neuron_ids = neuron_ids[core_mask_np[neuron_ids]]
+
+            neuron_ids = tf.cast(neuron_ids, dtype=tf.int32)
+            target_firing_rates[key]['neuron_ids'] = neuron_ids
+            type_n_neurons = len(neuron_ids)
+            sorted_target_rates = losses.sample_firing_rates(value["rates"], type_n_neurons, flags.seed)
+            target_firing_rates[key]['sorted_target_rates'] = tf.cast(sorted_target_rates, dtype=tf.float32) 
 
         voltage_regularizer = losses.VoltageRegularization(rsnn_layer.cell, flags.voltage_cost, dtype=dtype, core_mask=core_mask)
         voltage_loss = voltage_regularizer(rsnn_layer.output[0][1]) 
@@ -317,17 +323,17 @@ def main(_):
 
         grad = tape.gradient(_loss, model.trainable_variables)
         for g, v in zip(grad, model.trainable_variables):
-            tf.print(f'{v.name} optimization')
-            tf.print('First Var / grad: ', v[0].dtype, g[0].dtype)
+            # tf.print(f'{v.name} optimization')
+            # tf.print('First Var / grad: ', v[0].dtype, g[0].dtype)
             # tf.print('First Var / grad: ', v[0], g[0])
-            tf.print('Loss, total_gradients : ', _loss, tf.reduce_sum(tf.math.abs(g)))
-            tf.print(g)
-            tf.print(v)
+            # tf.print('Loss, total_gradients : ', _loss, tf.reduce_sum(tf.math.abs(g)))
+            # tf.print(g)
+            # tf.print(v)
             with tf.control_dependencies([_op]):
                 _op = optimizer.apply_gradients([(g, v)])
 
             # print the number of nan values in the gradients and the variables.
-            tf.print(v)
+            # tf.print(v)
             # tf.print(f'Number of nan in gradients: {tf.reduce_sum(tf.cast(tf.math.is_nan(g), tf.float32))}')
             # tf.print(f'Number of nan in variables: {tf.reduce_sum(tf.cast(tf.math.is_nan(v), tf.float32))}')
 
@@ -466,7 +472,8 @@ def main(_):
             train_values = [a.result().numpy() for a in [train_accuracy, train_loss, train_firing_rate, 
                                                          train_rate_loss, train_voltage_loss, train_osi_loss]]
 
-            callbacks.on_step_end(train_values, y, verbose=False)
+            # callbacks.on_step_end(train_values, y, verbose=False)
+            callbacks.on_step_end(train_values, y, verbose=True)
 
         # tf.profiler.experimental.stop() 
 

v1_model_utils/loss_functions.py
@@ -59,7 +59,7 @@ def compute_spike_rate_target_loss(_spikes, target_rates, core_mask=None, dtype=
     return tf.reduce_sum(losses, axis=0)
 
 def compute_spike_rate_distribution_loss(_rates, target_rate, dtype=tf.float32):
-    tf.print(f"target_rate: {target_rate}")
+    # tf.print(f"target_rate: {target_rate}")
     # ind = tf.range(target_rate.shape[0])
     # rand_ind = tf.random.shuffle(ind)
     # _rate = tf.gather(_rates, rand_ind)
@@ -118,12 +118,18 @@ class VoltageRegularization:
         self._dtype = dtype
         self._core_mask = core_mask
 
+        if core_mask is None:
+            self.voltage_offset = self._cell.voltage_offset
+            self.voltage_scale = self._cell.voltage_scale
+        else:
+            self.voltage_offset = tf.boolean_mask(self._cell.voltage_offset, core_mask)
+            self.voltage_scale = tf.boolean_mask(self._cell.voltage_scale, core_mask)
+
     def __call__(self, voltages):
         if self._core_mask is not None:
             voltages = tf.boolean_mask(voltages, self._core_mask, axis=2)
             
-        voltage_32 = (voltages - self._cell.voltage_offset) / self._cell.voltage_scale
-        voltage_32 = tf.cast(voltage_32, self._dtype)
+        voltage_32 = (voltages - self.voltage_offset) / self.voltage_scale
         v_pos = tf.square(tf.nn.relu(voltage_32 - 1.0))
         v_neg = tf.square(tf.nn.relu(-voltage_32 + 1.0))
         voltage_loss = tf.reduce_mean(tf.reduce_sum(v_pos + v_neg, -1)) * self._voltage_cost
@@ -154,7 +160,7 @@ class OrientationSelectivityLoss:
         delta_angle = tf.expand_dims(stim_angle, axis=1) - tuning_angle
         delta_angle = tf.where(delta_angle > 90, delta_angle - 180, delta_angle)
         delta_angle = tf.where(delta_angle < -90, delta_angle + 180, delta_angle)
-        # # do it twice to make sure
+        # # do it twice to make sure everything is between -90 and 90.
         delta_angle = tf.where(delta_angle > 90, delta_angle - 180, delta_angle)
         delta_angle = tf.where(delta_angle < -90, delta_angle + 180, delta_angle)
 
@@ -174,10 +180,11 @@ class OrientationSelectivityLoss:
 
         delta_angle = self.calculate_delta_angle(angle, self._tuning_angles)
         # sum spikes in _z, and multiply with delta_angle.
-        sum_angle = tf.reduce_mean(spikes, axis=[1]) * delta_angle
-        # make a huber loss for this.
-        # angle_loss = tf.keras.losses.Huber(delta=1, reduction=tf.keras.losses.Reduction.SUM)(sum_angle, tf.zeros_like(sum_angle))
-        angle_loss = tf.reduce_mean(tf.abs(sum_angle))
-        # it might be nice to add regularization of weights
-        # rec_weight_loss = rec_weight_regularizer(rsnn_layer.cell.recurrent_weight_values)
+        mean_spikes = tf.reduce_mean(spikes, axis=[1])
+        mean_angle = mean_spikes * delta_angle
+        # Here, the expected value with random firing to subtract
+        # (this prevents the osi loss to drive the firing rates to go to zero.)
+        expected_sum_angle = tf.reduce_mean(mean_spikes) * 45
+        
+        angle_loss = tf.reduce_mean(tf.abs(mean_angle)) - expected_sum_angle
         return angle_loss * self._osi_cost