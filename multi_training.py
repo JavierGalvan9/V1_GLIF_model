bb0e912195dd9a7b8ef000f12015b2d928dea1a0
shinya.ito <shinya.ito@alleninstitute.org>
2024-06-12 14:14:35 -0700

    Fixing the initial EMA

--- multi_training.py
+++ multi_training.py
@@ -239,7 +239,8 @@ def main(_):
                 data_loaded = pkl.load(f)
                 v1_ema = tf.Variable(data_loaded['v1_ema'], trainable=False, name='V1_EMA')
         else:
-            v1_ema = tf.Variable(tf.ones(shape=(flags.neurons,)), trainable=False, name='V1_EMA')
+            # 3 Hz is near the average FR of cortex
+            v1_ema = tf.Variable(0.003 * tf.ones(shape=(flags.neurons,)), trainable=False, name='V1_EMA')
 
         # here we need information of the layer mask for the OSI loss
         if flags.osi_loss_method == 'neuropixels_fr':
