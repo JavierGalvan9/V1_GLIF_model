Solved OOM bug + misc mixed precision

lgn_model/lgn.py
@@ -399,7 +399,7 @@ class LGN(object):
             self.dom_temporal_kernels = tf.constant(dom_temporal_kernels, dtype=dtype)
             self.non_dom_temporal_kernels = tf.constant(non_dom_temporal_kernels, dtype=dtype)
             # self.gaussian_filters = gaussian_filters
-            self.gaussian_filters = [tf.constant(gf, dtype=dtype) for gf in self.gaussian_filters]
+            self.gaussian_filters = [tf.cast(gf, dtype=dtype) for gf in gaussian_filters]
             self.spatial_range_indices = spatial_range_indices
             self.sorted_neuron_ids_indices = sorted_neuron_ids_indices
             # self.actual_spatial_range = actual_spatial_range
@@ -424,7 +424,7 @@ class LGN(object):
     def spatial_response(self, movie, bmtk_compat=True):
 
         if not isinstance(movie, tf.Tensor):
-            movie = tf.constant(movie, dtype=dtype)
+            movie = tf.constant(movie, dtype=self.dtype)
             print(f'Movie type: {type(movie)}')
        
         all_spatial_responses = []

multi_training_single_gpu_split.py
@@ -30,6 +30,7 @@ import stim_dataset
 
 from time import time
 import ctypes.util
+import random
 
 
 print("--- CUDA version: ", tf.sysconfig.get_build_info()["cuda_version"])
@@ -58,6 +59,7 @@ def main(_):
     # Set the seeds for reproducibility
     np.random.seed(flags.seed)
     tf.random.set_seed(flags.seed) 
+    random.seed(flags.seed)
 
     logdir = flags.ckpt_dir
     if logdir == '':
@@ -77,16 +79,22 @@ def main(_):
         flag_str = logdir.split(os.path.sep)[-2]
 
     # Can be used to try half precision training
-    if flags.float16:
-        # policy = mixed_precision.Policy("mixed_float16")
-        # mixed_precision.set_policy(policy)
+    if flags.dtype=='float16':
         if version.parse(tf.__version__) < version.parse("2.4.0"):
             policy = mixed_precision.Policy("mixed_float16")
             mixed_precision.set_policy(policy)
         else:
             mixed_precision.set_global_policy('mixed_float16')
         dtype = tf.float16
-        print('Mixed precision enabled!')
+        print('Mixed precision (float16) enabled!')
+    elif flags.dtype=='bfloat16':
+        if version.parse(tf.__version__) < version.parse("2.4.0"):
+            policy = mixed_precision.Policy("mixed_bfloat16")
+            mixed_precision.set_policy(policy)
+        else:
+            mixed_precision.set_global_policy('mixed_bfloat16')
+        dtype = tf.bfloat16
+        print('Mixed precision (bfloat16) enabled!')
     else:
         dtype = tf.float32
 
@@ -222,11 +230,16 @@ def main(_):
             print(f"No checkpoint found in {flags.ckpt_dir} or {flags.restore_from}. Starting from scratch...\n")
             checkpoint = None
 
+        #Enable loss scaling for training float16 model
+        if flags.dtype == 'float16':
+            optimizer = mixed_precision.LossScaleOptimizer(optimizer) # to prevent suffering from underflow gradients when using tf.float16
+
         # model_variables_dict['Best'] =  {var.name: var.numpy().astype(np.float16) for var in model.trainable_variables}
         model_variables_dict['Best'] = {
             var.name: var.numpy().astype(np.float16) if len(var.shape) == 1 else var[:, 0].numpy().astype(np.float16)
             for var in model.trainable_variables
         }
+        print(f"Model variables stored in dictionary\n")
         
         ### BUILD THE LOSS AND REGULARIZER FUNCTIONS ###
         # Create rate and voltage regularizers
@@ -251,7 +264,7 @@ def main(_):
         prediction_layer = model.get_layer('prediction')
 
         ### RECURRENT REGULARIZERS ###
-        rec_weight_regularizer = losses.StiffRegularizer(flags.recurrent_weight_regularization, network, penalize_relative_change=True, dtype=dtype)
+        rec_weight_regularizer = losses.StiffRegularizer(flags.recurrent_weight_regularization, network, penalize_relative_change=True, dtype=tf.float32)
         model.add_loss(lambda: rec_weight_regularizer(rsnn_layer.cell.recurrent_weight_values))
         # rec_weight_regularizer = losses.StiffRegularizer(flags.recurrent_weight_regularization, rsnn_layer.cell.recurrent_weight_values)
         # rec_weight_l2_regularizer = losses.L2Regularizer(flags.recurrent_weight_regularization, rsnn_layer.cell.recurrent_weight_values)
@@ -259,25 +272,25 @@ def main(_):
         ### EVOKED RATES REGULARIZERS ###
         rate_core_mask = None if flags.all_neuron_rate_loss else core_mask
         evoked_rate_regularizer = losses.SpikeRateDistributionTarget(network, spontaneous_fr=False, rate_cost=flags.rate_cost, pre_delay=delays[0], post_delay=delays[1], 
-                                                                    data_dir=flags.data_dir, core_mask=rate_core_mask, seed=flags.seed, dtype=dtype)
+                                                                    data_dir=flags.data_dir, core_mask=rate_core_mask, seed=flags.seed, dtype=tf.float32)
         model.add_loss(lambda: evoked_rate_regularizer(rsnn_layer.output[0][0]))
         
         ### SPONTANEOUS RATES REGULARIZERS ###
         spont_rate_regularizer = losses.SpikeRateDistributionTarget(network, spontaneous_fr=True, rate_cost=flags.rate_cost, pre_delay=delays[0], post_delay=delays[1], 
-                                                                    data_dir=flags.data_dir, core_mask=rate_core_mask, seed=flags.seed, dtype=dtype)
+                                                                    data_dir=flags.data_dir, core_mask=rate_core_mask, seed=flags.seed, dtype=tf.float32)
         model.add_loss(lambda: spont_rate_regularizer(rsnn_layer.output[0][0]))
         # evoked_rate_regularizer = models.SpikeRateDistributionRegularization(target_firing_rates, flags.rate_cost)
 
         ### VOLTAGE REGULARIZERS ###
-        # voltage_regularizer = losses.VoltageRegularization(rsnn_layer.cell, voltage_cost=flags.voltage_cost, dtype=dtype, core_mask=core_mask)
-        voltage_regularizer = losses.VoltageRegularization(rsnn_layer.cell, voltage_cost=flags.voltage_cost, dtype=dtype)
+        # voltage_regularizer = losses.VoltageRegularization(rsnn_layer.cell, voltage_cost=flags.voltage_cost, dtype=tf.float32, core_mask=core_mask)
+        voltage_regularizer = losses.VoltageRegularization(rsnn_layer.cell, voltage_cost=flags.voltage_cost, dtype=tf.float32)
         model.add_loss(lambda: voltage_regularizer(rsnn_layer.output[0][1]))
 
         ### SYNCHRONIZATION REGULARIZERS ###
-        evoked_sync_loss = losses.SynchronizationLoss(network, sync_cost=flags.sync_cost, core_mask=core_mask, t_start=0.2, t_end=flags.seq_len/1000, n_samples=500, dtype=dtype, session='evoked', data_dir='Synchronization_data')
+        evoked_sync_loss = losses.SynchronizationLoss(network, sync_cost=flags.sync_cost, core_mask=core_mask, t_start=0.2, t_end=flags.seq_len/1000, n_samples=500, dtype=tf.float32, session='evoked', data_dir='Synchronization_data')
         model.add_loss(lambda: evoked_sync_loss(rsnn_layer.output[0][0]))
 
-        spont_sync_loss = losses.SynchronizationLoss(network, sync_cost=flags.sync_cost, core_mask=core_mask, t_start=0.2, t_end=flags.seq_len/1000, n_samples=500, dtype=dtype, session='spont', data_dir='Synchronization_data')
+        spont_sync_loss = losses.SynchronizationLoss(network, sync_cost=flags.sync_cost, core_mask=core_mask, t_start=0.2, t_end=flags.seq_len/1000, n_samples=500, dtype=tf.float32, session='spont', data_dir='Synchronization_data')
         model.add_loss(lambda: spont_sync_loss(rsnn_layer.output[0][0]))
 
         ### OSI / DSI LOSSES ###
@@ -290,7 +303,7 @@ def main(_):
                 v1_ema = tf.Variable(data_loaded['v1_ema'], trainable=False, name='V1_EMA')
         else:
             # 3 Hz is near the average FR of cortex
-            v1_ema = tf.Variable(tf.constant(0.003, shape=(flags.neurons,), dtype=dtype), trainable=False, name='V1_EMA')
+            v1_ema = tf.Variable(tf.constant(0.003, shape=(flags.neurons,), dtype=tf.float32), trainable=False, name='V1_EMA')
             # v1_ema = tf.Variable(0.01 * tf.ones(shape=(flags.neurons,)), trainable=False, name='V1_EMA')
 
         # here we need information of the layer mask for the OSI loss
@@ -301,11 +314,11 @@ def main(_):
 
         OSI_DSI_Loss = losses.OrientationSelectivityLoss(network=network, osi_cost=flags.osi_cost, 
                                                         pre_delay=delays[0], post_delay=delays[1], 
-                                                        dtype=dtype, core_mask=core_mask,
+                                                        dtype=tf.float32, core_mask=core_mask,
                                                         method=flags.osi_loss_method,
                                                         subtraction_ratio=flags.osi_loss_subtraction_ratio,
                                                         layer_info=layer_info)
-        placeholder_angle = tf.constant(0, dtype=dtype, shape=(1, 1))
+        placeholder_angle = tf.constant(0, dtype=tf.float32, shape=(1, 1))
         model.add_loss(lambda: OSI_DSI_Loss(rsnn_layer.output[0][0], placeholder_angle, trim=True, normalizer=v1_ema))
         # osi_dsi_loss = OSI_DSI_Loss(rsnn_layer.output[0][0], tf.constant(0, dtype=tf.float32, shape=(1,1)), trim=True) 
 
@@ -319,20 +332,20 @@ def main(_):
         ### ANNULUS REGULARIZERS ###
         if annulus_mask is not None:
             annulus_spont_rate_regularizer = losses.SpikeRateDistributionTarget(network, spontaneous_fr=True, rate_cost=0.1*flags.rate_cost, pre_delay=delays[0], post_delay=delays[1],
-                                                                                data_dir=flags.data_dir, core_mask=annulus_mask, seed=flags.seed, dtype=dtype)
+                                                                                data_dir=flags.data_dir, core_mask=annulus_mask, seed=flags.seed, dtype=tf.float32)
             model.add_loss(lambda: annulus_spont_rate_regularizer(rsnn_layer.output[0][0]))
             annulus_evoked_rate_regularizer = losses.SpikeRateDistributionTarget(network, spontaneous_fr=False, rate_cost=0.1*flags.rate_cost, pre_delay=delays[0], post_delay=delays[1],
-                                                                                data_dir=flags.data_dir, core_mask=annulus_mask, seed=flags.seed, dtype=dtype)
+                                                                                data_dir=flags.data_dir, core_mask=annulus_mask, seed=flags.seed, dtype=tf.float32)
             model.add_loss(lambda: annulus_evoked_rate_regularizer(rsnn_layer.output[0][0]))
 
             # Add OSI/DSI regularizer for the annulus
             annulus_OSI_DSI_Loss = losses.OrientationSelectivityLoss(network=network, osi_cost=0.1*flags.osi_cost,
                                                                     pre_delay=delays[0], post_delay=delays[1], 
-                                                                    dtype=dtype, core_mask=annulus_mask,
+                                                                    dtype=tf.float32, core_mask=annulus_mask,
                                                                     method=flags.osi_loss_method,
                                                                     subtraction_ratio=flags.osi_loss_subtraction_ratio,
                                                                     layer_info=layer_info)
-            placeholder_angle = tf.constant(0, dtype=dtype, shape=(1, 1))
+            placeholder_angle = tf.constant(0, dtype=tf.float32, shape=(1, 1))
             model.add_loss(lambda: annulus_OSI_DSI_Loss(rsnn_layer.output[0][0], placeholder_angle, trim=True, normalizer=v1_ema))
 
         extractor_model = tf.keras.Model(inputs=model.inputs,
@@ -387,8 +400,12 @@ def main(_):
         seq_len = tf.shape(_x)[1]
         dummy_zeros = tf.zeros((flags.batch_size, seq_len, flags.neurons), dtype)
         _out, _p, _ = extractor_model((_x, dummy_zeros, _initial_state))
-
         _z, _v, _ = _out[0]
+
+        if flags.dtype != 'float32':
+            _z = tf.cast(_z, tf.float32)
+            _v = tf.cast(_v, tf.float32)
+
         # update state_variables with the new model state
         new_state = tuple(_out[1:])
         tf.nest.map_structure(lambda a, b: a.assign(b), state_variables, new_state)
@@ -401,19 +418,19 @@ def main(_):
 
         if spontaneous:
             rate_loss = spont_rate_regularizer(_z, trim)
-            osi_dsi_loss = tf.constant(0.0, dtype=dtype)
+            osi_dsi_loss = tf.constant(0.0, dtype=tf.float32)
             regularizers_loss = rec_weight_regularizer(rsnn_layer.cell.recurrent_weight_values)
             sync_loss = spont_sync_loss(_z, trim)
         else:
             rate_loss = evoked_rate_regularizer(_z, trim)
             osi_dsi_loss = OSI_DSI_Loss(_z, _y, trim, normalizer=v1_ema)
-            regularizers_loss = tf.constant(0.0, dtype=dtype)
+            regularizers_loss = tf.constant(0.0, dtype=tf.float32)
             sync_loss = evoked_sync_loss(_z, trim)
 
         if annulus_mask is not None:
             if spontaneous:
                 annulus_rate_loss = annulus_spont_rate_regularizer(_z, trim)
-                annulus_osi_dsi_loss = tf.constant(0.0, dtype=dtype)
+                annulus_osi_dsi_loss = tf.constant(0.0, dtype=tf.float32)
             else:
                 annulus_rate_loss = annulus_evoked_rate_regularizer(_z, trim)
                 annulus_osi_dsi_loss = annulus_OSI_DSI_Loss(_z, _y, trim, normalizer=v1_ema)
@@ -447,8 +464,15 @@ def main(_):
         ### Forward propagation of the model
         with tf.GradientTape() as tape:
             _out, _p, _loss, _aux, _ = roll_out(_x, _y, _w, trim=trim, spontaneous=spontaneous)
+            # Scale the loss for float16         
+            if flags.dtype=='float16':
+                _scaled_loss = optimizer.get_scaled_loss(_loss)
 
-        grad = tape.gradient(_loss, model.trainable_variables)
+        if flags.dtype=='float16':
+            scaled_gradients = tape.gradient(_scaled_loss, model.trainable_variables)
+            grad = optimizer.get_unscaled_gradients(scaled_gradients)
+        else:
+            grad = tape.gradient(_loss, model.trainable_variables)
 
         return _loss, _aux, _out, _p, grad
 
@@ -594,11 +618,11 @@ def main(_):
     y_spontaneous = tf.constant(0, dtype=dtype, shape=(1,1)) 
     w_spontaneous = tf.constant(flags.seq_len, dtype=dtype, shape=(1,1))
     spontaneous_lgn_firing_rates = next(iter(gray_data_set))   
-    spontaneous_lgn_firing_rates = tf.constant(spontaneous_lgn_firing_rates, dtype=dtype)
+    # spontaneous_lgn_firing_rates = tf.constant(spontaneous_lgn_firing_rates, dtype=dtype)
     # load LGN spontaneous firing rates 
     spontaneous_prob = 1 - tf.exp(-spontaneous_lgn_firing_rates / 1000.)
 
-    @tf.function(jit_compile=True)
+    @tf.function
     def generate_spontaneous_spikes(spontaneous_prob):
         x_spontaneous = tf.random.uniform(tf.shape(spontaneous_prob), dtype=dtype) < spontaneous_prob
         return x_spontaneous
@@ -629,7 +653,7 @@ def main(_):
         else:
             raise ValueError(f"Invalid reset_type: {reset_type}")
 
-    @tf.function
+    # @tf.function
     def distributed_reset_state(reset_type, gray_state=None):
         if reset_type == 'gray':
             if gray_state is None:
@@ -831,6 +855,7 @@ if __name__ == '__main__':
     absl.app.flags.DEFINE_string('delays', '100,0', '')
     # absl.app.flags.DEFINE_string('neuron_model', 'GLIF3', '')
     absl.app.flags.DEFINE_string('scale', '2,2', '')
+    absl.app.flags.DEFINE_string('dtype', 'float32', '')
 
     absl.app.flags.DEFINE_string('optimizer', 'adam', '')
     absl.app.flags.DEFINE_float('learning_rate', .001, '')
@@ -899,7 +924,7 @@ if __name__ == '__main__':
     # absl.app.flags.DEFINE_boolean('use_rand_ini_w', True, '')
     # absl.app.flags.DEFINE_boolean('use_decoded_noise', True, '')
     # absl.app.flags.DEFINE_boolean('from_lgn', True, '')
-    absl.app.flags.DEFINE_boolean("float16", False, "")
+    # absl.app.flags.DEFINE_boolean("float16", False, "")
 
     absl.app.flags.DEFINE_integer("cue_duration", 40, "")
     absl.app.flags.DEFINE_boolean("hard_reset", False, "")

osi_dsi_estimator.py
@@ -24,6 +24,7 @@ from v1_model_utils.callbacks import OsiDsiCallbacks, printgpu
 import stim_dataset
 from time import time
 import ctypes.util
+import random
 
 
 print("--- CUDA version: ", tf.sysconfig.get_build_info()["cuda_version"])
@@ -53,6 +54,7 @@ def main(_):
     # Set the seeds for reproducibility
     np.random.seed(flags.seed)
     tf.random.set_seed(flags.seed)
+    random.seed(flags.seed)
 
     # Load the checkpoint model if it exists
     logdir = flags.ckpt_dir
@@ -75,14 +77,22 @@ def main(_):
         current_epoch = (flags.run_session + 1) * flags.n_epochs
         
     # Can be used to try half precision training
-    if flags.float16:
+    if flags.dtype=='float16':
         if version.parse(tf.__version__) < version.parse("2.4.0"):
             policy = mixed_precision.Policy("mixed_float16")
             mixed_precision.set_policy(policy)
         else:
             mixed_precision.set_global_policy('mixed_float16')
         dtype = tf.float16
-        print('Mixed precision enabled!')
+        print('Mixed precision (float16) enabled!')
+    elif flags.dtype=='bfloat16':
+        if version.parse(tf.__version__) < version.parse("2.4.0"):
+            policy = mixed_precision.Policy("mixed_bfloat16")
+            mixed_precision.set_policy(policy)
+        else:
+            mixed_precision.set_global_policy('mixed_bfloat16')
+        dtype = tf.bfloat16
+        print('Mixed precision (bfloat16) enabled!')
     else:
         dtype = tf.float32
 
@@ -318,7 +328,7 @@ def main(_):
             else:
                 raise ValueError(f"Invalid reset_type: {reset_type}")
 
-        @tf.function
+        # @tf.function
         def distributed_reset_state(reset_type, gray_state=None):
             if reset_type == 'gray':
                 strategy.run(reset_state, args=(reset_type, gray_state))
@@ -362,6 +372,9 @@ def main(_):
                 for gpu_id in range(len(strategy.extended.worker_devices)):
                     printgpu(gpu_id=gpu_id)
 
+                if not flags.calculate_osi_dsi:
+                    break
+
         # Do the OSI/DSI analysis       
         if flags.calculate_osi_dsi:
             callbacks.osi_dsi_analysis(spikes, DG_angles)
@@ -379,6 +392,7 @@ if __name__ == '__main__':
     absl.app.flags.DEFINE_string('delays', '100,0', '')
     absl.app.flags.DEFINE_string('scale', '2,2', '')
     absl.app.flags.DEFINE_string('optimizer', 'adam', '')
+    absl.app.flags.DEFINE_string('dtype', 'float32', '')
 
     absl.app.flags.DEFINE_float('learning_rate', .01, '')
     absl.app.flags.DEFINE_float('rate_cost', 100., '')
@@ -418,7 +432,7 @@ if __name__ == '__main__':
     absl.app.flags.DEFINE_integer('validation_examples', 16, '')
     absl.app.flags.DEFINE_integer('n_trials_per_angle', 10, '')
 
-    absl.app.flags.DEFINE_boolean('float16', False, '')
+    # absl.app.flags.DEFINE_boolean('float16', False, '')
     absl.app.flags.DEFINE_boolean('caching', True, '')
     absl.app.flags.DEFINE_boolean('core_only', False, '')
     absl.app.flags.DEFINE_boolean('core_loss', False, '')

stim_dataset.py
@@ -52,7 +52,7 @@ def make_drifting_grating_stimulus(row_size=80, col_size=120, moving_flag=True, 
     data = contrast * tf.sin(2 * np.pi * (cpd * xy + temporal_f * tt) + phase_rad)
 
     if moving_flag: # decide whether the gratings drift or they are static 
-        return tf.cast(data, dtype) 
+        return data
     else:
         return tf.tile(data[0][tf.newaxis, ...], (image_duration, 1, 1))
 
@@ -87,11 +87,10 @@ def generate_drifting_grating_tuning(orientation=None, temporal_f=2, cpd=0.04, c
                 if regular:
                     theta = (theta + 45) % 360
                 else:
-                    theta = np.random.uniform(0, 360)
+                    theta = tf.random.uniform(shape=(1,), minval=0, maxval=360, dtype=dtype)
             else:
                 theta = orientation
 
-            # theta = tf.cast(theta, dtype)
             if rotation == "cw":
                 mov_theta = theta
             if rotation == "ccw":
@@ -102,8 +101,6 @@ def generate_drifting_grating_tuning(orientation=None, temporal_f=2, cpd=0.04, c
 
             # Generate a random phase
             phase = tf.random.uniform(shape=(1,), minval=0, maxval=360, dtype=dtype)
-            # phase = np.random.uniform(0, 360)
-            mov_theta = tf.constant(mov_theta, dtype=dtype)
 
             movie = make_drifting_grating_stimulus(moving_flag=True, image_duration=duration, cpd=cpd, temporal_f=temporal_f, theta=mov_theta, 
                                                    phase=phase, contrast=contrast, dtype=dtype)
@@ -116,7 +113,8 @@ def generate_drifting_grating_tuning(orientation=None, temporal_f=2, cpd=0.04, c
             # process temporal filters and get firing rates
             firing_rates = lgn.firing_rates_from_spatial(*spatial)
             if return_firing_rates:
-                yield tf.constant(firing_rates, dtype=dtype, shape=(seq_len, n_input))
+                # yield tf.constant(firing_rates, dtype=dtype, shape=(seq_len, n_input))
+                yield firing_rates
 
             else:
                 del spatial
@@ -131,7 +129,7 @@ def generate_drifting_grating_tuning(orientation=None, temporal_f=2, cpd=0.04, c
                     _z = tf.random.uniform(tf.shape(_p), dtype=dtype) < _p
                 del _p
 
-                yield _z, tf.constant(theta, dtype=dtype, shape=(1,)), tf.constant(contrast, dtype=dtype, shape=(1,)), tf.constant(duration, dtype=dtype, shape=(1,))
+                yield _z, theta, tf.constant(contrast, dtype=dtype, shape=(1,)), tf.constant(duration, dtype=dtype, shape=(1,))
                 # yield _z, np.array([theta], dtype=np.float32)
 
     if return_firing_rates:

v1_model_utils/callbacks.py
@@ -33,7 +33,7 @@ def printgpu(gpu_id=0):
         used, free, total = [float(x)/1024 for x in result.stdout.strip().split(',')]
         print(f"    Total GPU Memory Usage: Used: {used:.2f} GiB, Free: {free:.2f} GiB, Total: {total:.2f} GiB")
 
-        return current, peak
+        # return current, peak
 
 def compose_str(metrics_values):
         _acc, _loss, _rate, _rate_loss, _voltage_loss, _regularizers_loss, _osi_dsi_loss, _sync_loss = metrics_values
@@ -838,6 +838,22 @@ class Callbacks:
 
     def on_epoch_end(self, x, v1_spikes, y, metric_values, bkg_noise=None, verbose=True,
                     x_spont=None, v1_spikes_spont=None):
+        
+        if v1_spikes.dtype == tf.float16:
+            v1_spikes = v1_spikes.numpy().astype(np.float32)
+            x = x.numpy().astype(np.float32)
+            y = y.numpy().astype(np.float32)
+            if x_spont is not None:
+                x_spont = x_spont.numpy().astype(np.float32)
+                v1_spikes_spont = v1_spikes_spont.numpy().astype(np.float32)
+        else:
+            v1_spikes = v1_spikes.numpy()
+            x = x.numpy()
+            y = y.numpy()
+            if x_spont is not None:
+                x_spont = x_spont.numpy()
+                v1_spikes_spont = v1_spikes_spont.numpy()
+
         self.step = 0
         if self.initial_metric_values is None:
             self.initial_metric_values = metric_values
@@ -972,9 +988,6 @@ class Callbacks:
         plt.close()
     
     def plot_raster(self, x, v1_spikes, y):
-        v1_spikes = v1_spikes.numpy()
-        x = x.numpy()
-        y = y.numpy()
         seq_len = v1_spikes.shape[1]
         images_dir = os.path.join(self.logdir, 'Raster_plots')
         os.makedirs(images_dir, exist_ok=True)
@@ -994,8 +1007,8 @@ class Callbacks:
 
     def composed_raster(self, x, v1_spikes, x_spont, v1_spikes_spont, y, plot_core_only=True):
         # concatenate the normal and spontaneous arrays
-        x = np.concatenate((x_spont.numpy(), x.numpy()), axis=1)
-        v1_spikes = np.concatenate((v1_spikes_spont.numpy(), v1_spikes.numpy()), axis=1)
+        x = np.concatenate((x_spont, x), axis=1)
+        v1_spikes = np.concatenate((v1_spikes_spont, v1_spikes), axis=1)
         seq_len = v1_spikes.shape[1]
         images_dir = os.path.join(self.logdir, 'Raster_plots')
         if plot_core_only:
@@ -1018,8 +1031,8 @@ class Callbacks:
         graph(x, v1_spikes)
 
     def plot_lgn_activity(self, x, x_spont):
-        x = x.numpy()[0, :, :]
-        x_spont = x_spont.numpy()[0, :, :]
+        x = x[0, :, :]
+        x_spont = x_spont[0, :, :]
         x = np.concatenate((x_spont, x), axis=0)
         x_mean = np.mean(x, axis=1)
         plt.figure(figsize=(10, 5))
@@ -1032,7 +1045,7 @@ class Callbacks:
         plt.close()
 
     def plot_populations_activity(self, v1_spikes, v1_spikes_spont):
-        v1_spikes = np.concatenate((v1_spikes_spont.numpy(), v1_spikes.numpy()), axis=1)
+        v1_spikes = np.concatenate((v1_spikes_spont, v1_spikes), axis=1)
         seq_len = v1_spikes.shape[1]
         # Plot the mean firing rate of the population of neurons
         filename = f'Epoch_{self.epoch}'
@@ -1043,9 +1056,8 @@ class Callbacks:
         Population_activity(v1_spikes, plot_core_only=True, bin_size=10)
 
     def plot_mean_firing_rate_boxplot(self, v1_spikes, y):
-        v1_spikes = v1_spikes.numpy()
         seq_len = v1_spikes.shape[1]
-        DG_angles = y.numpy()
+        DG_angles = y
         boxplots_dir = os.path.join(self.logdir, f'Boxplots/{self.neuropixels_feature}')
         os.makedirs(boxplots_dir, exist_ok=True)        
         if self.neuropixels_feature == "Ave_Rate(Hz)":
@@ -1060,8 +1072,7 @@ class Callbacks:
         metrics_analysis(metrics=[self.neuropixels_feature], directory=boxplots_dir, filename=f'Epoch_{self.epoch}')    
                 
     def plot_spontaneous_boxplot(self, v1_spikes, y):
-        v1_spikes = v1_spikes.numpy()
-        DG_angles = y.numpy()
+        DG_angles = y
         seq_len = v1_spikes.shape[1]
         boxplots_dir = os.path.join(self.logdir, f'Boxplots/Spontaneous')
         os.makedirs(boxplots_dir, exist_ok=True)

v1_model_utils/loss_functions.py
@@ -113,11 +113,11 @@ class L2Regularizer(tf.keras.regularizers.Regularizer):
             x = tf.squeeze(x, axis=1)
 
         if self._target_mean_weights is None:
-            return self._strength * tf.reduce_mean(tf.square(x))
+            return tf.cast(self._strength * tf.reduce_mean(tf.square(x)), dtype=self._dtype)
         else:
             relative_deviation = x / self._target_mean_weights
             mse = self._strength * tf.reduce_mean(tf.square(relative_deviation))
-            return mse
+            return tf.cast(mse, dtype=self._dtype)
 
 
 def spike_trimming(spikes, pre_delay=50, post_delay=50, trim=True):
@@ -457,7 +457,7 @@ class SynchronizationLoss(Layer):
         # # Calculate mean, standard deviation, and SEM of the Fano factors
         fanos_mean = tf.reduce_mean(fanos, axis=1)
         # # Calculate MSE between the experimental and calculated Fano factors
-        mse_loss = tf.sqrt(tf.reduce_mean(tf.square(experimental_fanos_mean - fanos_mean)))
+        mse_loss = tf.reduce_mean(tf.square(experimental_fanos_mean - fanos_mean))
         # # Calculate the synchronization loss
         sync_loss = self._sync_cost * mse_loss
 
@@ -507,6 +507,7 @@ class OrientationSelectivityLoss:
         self._core_mask = core_mask
         self._method = method
         self._subtraction_ratio = subtraction_ratio  # only for crowd_spikes method
+        self._tf_pi = tf.constant(np.pi, dtype=dtype)
         if (self._core_mask is not None) and (self._method == "crowd_spikes" or self._method == "crowd_osi"):
             self.np_core_mask = self._core_mask.numpy()
             core_tuning_angles = network['tuning_angle'][self.np_core_mask]
@@ -703,7 +704,7 @@ class OrientationSelectivityLoss:
         delta_angle = tf.expand_dims(angle, axis=0) -  self._tuning_angles
         # i want the delta_angle to be within 0-360
         # clipped_delta_angle = tf.math.floormod(delta_angle, 360)
-        radians_delta_angle = delta_angle * (pi / 180)
+        radians_delta_angle = delta_angle * (self._tf_pi / 180)
 
         # sum spikes in _z, and multiply with delta_angle.
         rates = tf.reduce_mean(spikes, axis=[0, 1])
@@ -730,12 +731,11 @@ class OrientationSelectivityLoss:
         osi_loss_type = tf.math.square(osi_approx_type - self.osi_target_values)
         dsi_loss_type = tf.math.square(dsi_approx_type - self.dsi_target_values)
 
-        total_osi_loss0 = tf.reduce_sum(osi_loss_type * self.cell_type_count) / tf.reduce_sum(self.cell_type_count)
-        total_dsi_loss0 = tf.reduce_sum(dsi_loss_type * self.cell_type_count) / tf.reduce_sum(self.cell_type_count)
+        total_osi_loss = tf.reduce_sum(osi_loss_type * self.cell_type_count) / tf.reduce_sum(self.cell_type_count)
+        total_dsi_loss = tf.reduce_sum(dsi_loss_type * self.cell_type_count) / tf.reduce_sum(self.cell_type_count)
     
-        return (total_osi_loss0 + total_dsi_loss0) * self._osi_cost
+        return (total_osi_loss + total_dsi_loss) * self._osi_cost
     
-
     def __call__(self, spikes, angle, trim, normalizer=None):
 
         spikes = spike_trimming(spikes, pre_delay=self._pre_delay, post_delay=self._post_delay, trim=trim)

v1_model_utils/models.py
@@ -2,7 +2,6 @@ import numpy as np
 import tensorflow as tf
 import os 
 import pickle as pkl
-from numba import njit
 # import subprocess
 from . import other_v1_utils
 
@@ -49,6 +48,22 @@ def spike_gauss_16(v_scaled, sigma, amplitude):
         return [de_dv_scaled, tf.zeros_like(sigma), tf.zeros_like(amplitude)]
 
     return tf.identity(z_, name="spike_gauss"), grad
+
+
+@tf.custom_gradient
+def spike_gauss_b16(v_scaled, sigma, amplitude):
+    z_ = tf.greater(v_scaled, 0.)
+    z_ = tf.cast(z_, tf.bfloat16)
+
+    def grad(dy):
+        de_dz = dy
+        dz_dv_scaled = gauss_pseudo(v_scaled, sigma, amplitude)
+
+        de_dv_scaled = de_dz * dz_dv_scaled
+
+        return [de_dv_scaled, tf.zeros_like(sigma), tf.zeros_like(amplitude)]
+
+    return tf.identity(z_, name='spike_gauss'), grad
 
 # @tf.custom_gradient
 # def spike_slayer(v_scaled, sigma, amplitude):
@@ -141,7 +156,7 @@ def make_pre_ind_table(indices, n_source_neurons=197613):
     sorted_pre_inds, sorted_synapse_indices = tf.math.top_k(-pre_inds, k=n_syn)
     sorted_pre_inds = -sorted_pre_inds  # Undo the negation to get the sorted pre_inds
     # Count occurrences (out-degrees) for each presynaptic neuron using bincount
-    counts = tf.math.bincount(tf.cast(sorted_pre_inds, dtype=tf.int32), minlength=n_source_neurons)
+    counts = tf.math.bincount(sorted_pre_inds, minlength=n_source_neurons)
     # Create row_splits that covers all presynaptic neurons (0 to n_source_neurons)
     row_splits = tf.concat([[0], tf.cumsum(counts)], axis=0)
     # Create the RaggedTensor with empty rows for missing neurons
@@ -160,6 +175,7 @@ def get_new_inds_table(indices, non_zero_cols, pre_ind_table):
 
     return new_indices, all_inds
 
+
 class BackgroundNoiseLayer(tf.keras.layers.Layer):
     """
     This class calculates the input currents from the BKG noise by processing all timesteps at once."
@@ -167,14 +183,13 @@ class BackgroundNoiseLayer(tf.keras.layers.Layer):
     Each call takes 0.03 seconds for 600 ms of simulation.
 
     Returns:
-        _type_: input_currents (self._compute_dtype)
+        _type_: input_currents (self.compute_dtype)
     """
     def __init__(self, indices, weights, dense_shape,  
                  weights_factors, batch_size, seq_len,
                  bkg_firing_rate=250, n_bkg_units=100, 
-                 dtype=tf.float32, **kwargs):
+                 **kwargs):
         super().__init__(**kwargs)
-        self._dtype = dtype
         self._bkg_weights = weights
         self._bkg_indices = indices
         self._dense_shape = dense_shape
@@ -188,7 +203,7 @@ class BackgroundNoiseLayer(tf.keras.layers.Layer):
 
     def calculate_bkg_i_in(self, inputs):
         # This function performs the tensor multiplication to calculate the recurrent currents at each timestep
-        i_in = tf.TensorArray(dtype=self._dtype, size=self._n_syn_basis)
+        i_in = tf.TensorArray(dtype=self.compute_dtype, size=self._n_syn_basis)
         for r_id in range(self._n_syn_basis):
             weights_syn_receptors = self._bkg_weights * self._bkg_input_weights_factors[:, r_id]
             sparse_w_in = tf.sparse.SparseTensor(
@@ -202,8 +217,8 @@ class BackgroundNoiseLayer(tf.keras.layers.Layer):
                                                         adjoint_b=True
                                                     )
             # Optionally cast the output back to float16
-            if i_receptor.dtype != self._dtype:
-                i_receptor = tf.cast(i_receptor, dtype=self._dtype)
+            if i_receptor.dtype != self.compute_dtype:
+                i_receptor = tf.cast(i_receptor, dtype=self.compute_dtype)
 
             # Append i_receptor to the TensorArray
             i_in = i_in.write(r_id, i_receptor)
@@ -216,15 +231,15 @@ class BackgroundNoiseLayer(tf.keras.layers.Layer):
     def call(self, inp): # inp only provides the shape
         # Generate the background spikes
         seq_len = tf.shape(inp)[1]
-        # rest_of_brain = tf.random.poisson(shape=(self._batch_size, seq_len, self._n_bkg_units), 
-        #                                 lam=self._bkg_firing_rate * .001, 
-        #                                 seed=42,
-        #                                 dtype=self._compute_dtype) # this implementation is slower
-        rest_of_brain = tf.cast(tf.random.uniform(
-                (self._batch_size, seq_len, self._n_bkg_units)) < self._bkg_firing_rate * .001, 
-                tf.float32) # (1, 600, 100)
-
-        rest_of_brain = tf.reshape(rest_of_brain, (self._batch_size * seq_len, self._n_bkg_units)) # (batch_size*sequence_length, input_dim)
+        rest_of_brain = tf.random.poisson(shape=(self._batch_size * seq_len, self._n_bkg_units), 
+                                        lam=self._bkg_firing_rate * .001, 
+                                        dtype=self.variable_dtype) # this implementation is slower
+        
+        # rest_of_brain = tf.cast(tf.random.uniform(
+        #         (self._batch_size, seq_len, self._n_bkg_units)) < self._bkg_firing_rate * .001, 
+        #         tf.float32) # (1, 600, 100)
+        # rest_of_brain = tf.reshape(rest_of_brain, (self._batch_size * seq_len, self._n_bkg_units)) # (batch_size*sequence_length, input_dim)
+       
         # Create a TensorArray to save the results for every receptor type
         noise_input = self.calculate_bkg_i_in(rest_of_brain) # (5, 65871, 600)
         noise_input = tf.transpose(noise_input) # (600, 50000, 5) # New shape (3000, 65871, 5)
@@ -236,14 +251,13 @@ class BackgroundNoiseLayer(tf.keras.layers.Layer):
 
 class LGNInputLayerCell(tf.keras.layers.Layer):
     def __init__(self, indices, weights, dense_shape, weights_factors,
-                 dtype=tf.float32, **kwargs):
+                 **kwargs):
         super().__init__(**kwargs)
         self._indices = indices
         self._input_weights = weights
         self._input_weights_factors = weights_factors
         # self._n_syn_basis = weights_factors.shape[1]  # Number of receptors
         self._dense_shape = dense_shape
-        self._dtype = dtype
         # Precompute the synapses table
         self.pre_ind_table = make_pre_ind_table(indices, n_source_neurons=dense_shape[1])
 
@@ -256,7 +270,7 @@ class LGNInputLayerCell(tf.keras.layers.Layer):
     #     # If you have any trainable variables, initialize them here
     #     pass
 
-    @tf.function
+    # @tf.function
     def call(self, inputs_t, states):
         # inputs_t: Shape [batch_size, input_dim]
         batch_size = tf.shape(inputs_t)[0]
@@ -278,8 +292,8 @@ class LGNInputLayerCell(tf.keras.layers.Layer):
             num_segments=self._dense_shape[0]
         )
         # Optionally cast the output back to float16
-        if i_in.dtype != self._dtype:
-            i_in = tf.cast(i_in, dtype=self._dtype)
+        if i_in.dtype != self.compute_dtype:
+            i_in = tf.cast(i_in, dtype=self.compute_dtype)
 
         # Add batch dimension
         i_in = tf.expand_dims(i_in, axis=0)  # Shape: [1, n_post_neurons, n_syn_basis]
@@ -292,11 +306,11 @@ class LGNInputLayer(tf.keras.layers.Layer):
     Calculates input currents from the LGN by processing one timestep at a time using a custom RNN cell.
     """
     def __init__(self, indices, weights, dense_shape, weights_factors,
-                 dtype=tf.float32, **kwargs):
+                 **kwargs):
         super().__init__(**kwargs)
         self.input_cell = LGNInputLayerCell(
             indices, weights, dense_shape, weights_factors,
-            dtype=dtype, **kwargs
+            **kwargs
         )
         # Create the input RNN layer with the custom cell to recursively process all the inputs by timesteps
         self.input_rnn = tf.keras.layers.RNN(self.input_cell, return_sequences=True, return_state=False, name='lgn_rsnn')
@@ -336,6 +350,7 @@ class ClipConstraint(tf.keras.constraints.Constraint):
     def __call__(self, w):
         return tf.clip_by_value(w, self._lower_limit, self._upper_limit)
 
+
 class V1Column(tf.keras.layers.Layer):
     def __init__(
         self,
@@ -354,7 +369,7 @@ class V1Column(tf.keras.layers.Layer):
         pseudo_gauss=False,
         train_recurrent=True,
         train_recurrent_per_type=True,
-        train_input=True,
+        train_input=False,
         train_noise=True,
         hard_reset=True,
     ):
@@ -371,15 +386,15 @@ class V1Column(tf.keras.layers.Layer):
         # Define the other model variables
         self._node_type_ids = np.array(network["node_type_ids"])
         self._n_syn_basis = 5
-        self._dt = dt
-        self._recurrent_dampening = tf.cast(recurrent_dampening_factor, self._compute_dtype)
-        self._dampening_factor = tf.cast(dampening_factor, self._compute_dtype)
+        self._dt = tf.constant(dt, self.compute_dtype)
+        self._recurrent_dampening = tf.constant(recurrent_dampening_factor, self.compute_dtype)
+        self._dampening_factor = tf.constant(dampening_factor, self.compute_dtype)
         self._pseudo_gauss = pseudo_gauss
-        self._lr_scale = lr_scale
+        self._lr_scale = tf.constant(lr_scale, dtype=self.compute_dtype)
         self._spike_gradient = spike_gradient
         self._hard_reset = hard_reset
         self._n_neurons = int(network["n_nodes"])
-        self._gauss_std = tf.cast(gauss_std, self._compute_dtype)
+        self._gauss_std = tf.constant(gauss_std, self.compute_dtype)
         # Determine the membrane time decay constant
         tau = _params["C_m"] / _params["g"]
         membrane_decay = np.exp(-dt / tau)
@@ -389,11 +404,11 @@ class V1Column(tf.keras.layers.Layer):
         path='GLIF_network/synaptic_data/tau_basis.npy' # [0.7579732  1.33243834 2.34228851 4.11750046 7.23813909]
         tau_syns = np.load(path)
         syn_decay = np.exp(-dt / tau_syns)
-        syn_decay = tf.constant(syn_decay, dtype=self._compute_dtype)
+        syn_decay = tf.constant(syn_decay, dtype=self.compute_dtype)
         syn_decay = tf.tile(syn_decay, [self._n_neurons])
         self.syn_decay = tf.expand_dims(syn_decay, axis=0) # expand the dimension for processing different receptor types
         psc_initial = np.e / tau_syns
-        psc_initial = tf.constant(psc_initial, dtype=self._compute_dtype)
+        psc_initial = tf.constant(psc_initial, dtype=self.compute_dtype)
         psc_initial = tf.tile(psc_initial, [self._n_neurons])
         self.psc_initial = tf.expand_dims(psc_initial, axis=0) # expand the dimension for processing different receptor types
 
@@ -404,14 +419,14 @@ class V1Column(tf.keras.layers.Layer):
             return tf.gather(prop, self._node_type_ids)
     
         def _f(_v, trainable=False):
-            return tf.Variable(tf.cast(_gather(_v), self._compute_dtype), trainable=trainable)
+            return tf.Variable(tf.cast(_gather(_v), self.compute_dtype), trainable=trainable)
 
         def inv_sigmoid(_x):
             return tf.math.log(_x / (1 - _x))
 
         def custom_val(_v, trainable=False):
             _v = tf.Variable(
-                tf.cast(inv_sigmoid(_gather(_v)), self._compute_dtype),
+                tf.cast(inv_sigmoid(_gather(_v)), self.compute_dtype),
                 trainable=trainable,
             )
             def _g():
@@ -423,11 +438,11 @@ class V1Column(tf.keras.layers.Layer):
         self.t_ref = _f(_params["t_ref"])  # refractory time
         self.v_reset = _f(_params["V_reset"])
         self.asc_amps = _f(_params["asc_amps"], trainable=False)
-        _k = _params["k"]
+        _k = tf.cast(_params['k'], self.compute_dtype)
         # inverse sigmoid of the adaptation rate constant (1/ms)
         param_k, param_k_read = custom_val(_k, trainable=False)
         k = param_k_read()
-        self.exp_dt_k = tf.cast(tf.exp(-self._dt * k), self._compute_dtype)
+        self.exp_dt_k = tf.exp(-self._dt * k)
 
         self.v_th = _f(_params["V_th"])
         self.v_gap = self.v_reset - self.v_th
@@ -446,7 +461,7 @@ class V1Column(tf.keras.layers.Layer):
         with open(path, "rb") as f:
             syn_id_to_syn_weights_dict = pkl.load(f)
         synaptic_basis_weights = np.array(list(syn_id_to_syn_weights_dict.values()))
-        self.synaptic_basis_weights = tf.constant(synaptic_basis_weights, dtype=tf.float32)
+        self.synaptic_basis_weights = tf.constant(synaptic_basis_weights, dtype=self.variable_dtype)
 
         ### Network recurrent connectivity ###
         indices = np.array(network["synapses"]["indices"])
@@ -495,7 +510,7 @@ class V1Column(tf.keras.layers.Layer):
             name="sparse_recurrent_weights",
             constraint=SignedConstraint(recurrent_weight_positive),
             trainable=individual_training,
-            dtype=tf.float32
+            dtype=self.variable_dtype
         ) # shape = (n_synapses,)
 
         # prepare per_type variable, if required
@@ -509,7 +524,7 @@ class V1Column(tf.keras.layers.Layer):
                 name="recurrent_per_type_weights",
                 constraint=ClipConstraint(0.2, 5.0),
                 trainable=True,
-                dtype=tf.float32
+                dtype=self.variable_dtype
             ) # shape = (n_connection_types (21 * 21))
             # multiply this to the weights (this needs to be done in the loop)
         else:
@@ -540,12 +555,13 @@ class V1Column(tf.keras.layers.Layer):
 
         input_weight_positive = tf.Variable(
             input_weights >= 0.0, name="input_weights_sign", trainable=False)
+        
         self.input_weight_values = tf.Variable(
             input_weights * input_weight_scale / lr_scale,
             name="sparse_input_weights",
             constraint=SignedConstraint(input_weight_positive),
             trainable=train_input,
-            dtype=tf.float32
+            dtype=self.variable_dtype
         )
 
         input_syn_ids = tf.constant(input_syn_ids, dtype=tf.int32)
@@ -575,7 +591,7 @@ class V1Column(tf.keras.layers.Layer):
             name="rest_of_brain_weights", 
             constraint=SignedConstraint(bkg_input_weight_positive),
             trainable=train_noise,
-            dtype=tf.float32
+            dtype=self.variable_dtype
         )
 
         bkg_input_syn_ids = tf.constant(bkg_input_syn_ids, dtype=tf.int32)
@@ -607,13 +623,13 @@ class V1Column(tf.keras.layers.Layer):
         i_rec = tf.math.unsorted_segment_sum(
             sorted_data,
             sorted_segment_ids,
-            num_segments=self._n_neurons
+            num_segments=self.recurrent_dense_shape[0]
         )
 
-        if i_rec.dtype != self._compute_dtype:
-            i_rec = tf.cast(i_rec, dtype=self._compute_dtype)
+        if i_rec.dtype != self.compute_dtype:
+            i_rec = tf.cast(i_rec, dtype=self.compute_dtype)
         # Add batch dimension
-        i_rec = tf.expand_dims(i_rec, axis=0)
+        # i_rec = tf.expand_dims(i_rec, axis=0)
         i_rec = tf.reshape(i_rec, [1, -1])
             
         return i_rec
@@ -638,9 +654,9 @@ class V1Column(tf.keras.layers.Layer):
     
     def zero_state(self, batch_size, dtype=tf.float32):
         # The neurons membrane voltage start the simulation at their reset value
+        z0_buf = tf.zeros((batch_size, self._n_neurons * self.max_delay), dtype)
         v0 = tf.ones((batch_size, self._n_neurons), dtype) * tf.cast(
             self.v_th * 0.0 + 1.0 * self.v_reset, dtype)
-        z0_buf = tf.zeros((batch_size, self._n_neurons * self.max_delay), dtype)
         r0 = tf.zeros((batch_size, self._n_neurons), dtype)
         asc = tf.zeros((batch_size, self._n_neurons * 2), dtype)
         psc_rise0 = tf.zeros((batch_size, self._n_neurons * self._n_syn_basis), dtype)
@@ -648,7 +664,7 @@ class V1Column(tf.keras.layers.Layer):
 
         return z0_buf, v0, r0, asc, psc_rise0, psc0
 
-    @tf.function
+    # @tf.function
     def call(self, inputs, state, constants=None):
 
         # Get all the model inputs
@@ -658,9 +674,9 @@ class V1Column(tf.keras.layers.Layer):
         batch_size = tf.shape(bkg_noise)[0]
 
         if self._spike_gradient:
-            state_input = tf.zeros((1,), dtype=self._compute_dtype)
+            state_input = tf.zeros((1,), dtype=self.compute_dtype)
         else:
-            state_input = tf.zeros((4,), dtype=self._compute_dtype)
+            state_input = tf.zeros((4,), dtype=self.compute_dtype)
                 
         # Extract the network variables from the state
         z_buf, v, r, asc, psc_rise, psc = state
@@ -712,14 +728,16 @@ class V1Column(tf.keras.layers.Layer):
         # Generate the network spikes
         v_sc = (new_v - self.v_th) / self.normalizer
         if self._pseudo_gauss:
-            if self._compute_dtype == tf.bfloat16:
-                new_z = spike_function_b16(v_sc, self._dampening_factor)
-            elif self._compute_dtype == tf.float16:
+            if self.compute_dtype == tf.bfloat16:
+                new_z = spike_gauss_b16(v_sc, self._gauss_std, self._dampening_factor)
+            elif self.compute_dtype == tf.float16:
                 new_z = spike_gauss_16(v_sc, self._gauss_std, self._dampening_factor)
             else:
                 new_z = spike_gauss(v_sc, self._gauss_std, self._dampening_factor)
         else:
-            if self._compute_dtype == tf.float16:
+            if self.compute_dtype == tf.bfloat16:
+                new_z = spike_function_b16(v_sc, self._dampening_factor)
+            elif self.compute_dtype == tf.float16:
                 new_z = spike_function_16(v_sc, self._dampening_factor)
             else:
                 new_z = spike_function(v_sc, self._dampening_factor)
@@ -844,7 +862,6 @@ def create_model(
         cell.lgn_input_dense_shape,
         cell.input_weights_factors,
         # lr_scale=lr_scale,
-        dtype=dtype,
         name="input_layer",
     )(x)
 
@@ -857,7 +874,6 @@ def create_model(
         batch_size, 
         seq_len,
         # lr_scale=lr_scale,
-        dtype=dtype,
         name="noise_layer",
     )(x) # the input is provided just because in a Keras custom layer, the call method should accept input
 