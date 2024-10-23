25d25a04e7904a1383f440fbccfe6640b4b47827
Javier Galvan <jgalvan@ifisc.uib-csic.es>
2024-10-23 17:56:05 +0200

    Notebook for weights analysis

---
+++ general_utils/analysis_utils.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+from scipy import stats
+
+def freedman_diaconis(data, returnas="width"):
+    """
+    Use Freedman Diaconis rule to compute optimal histogram bin width. 
+    ``returnas`` can be one of "width" or "bins", indicating whether
+    the bin width or number of bins should be returned respectively. 
+
+
+    Parameters
+    ----------
+    data: np.ndarray
+        One-dimensional array.
+
+    returnas: {"width", "bins"}
+        If "width", return the estimated width for each histogram bin. 
+        If "bins", return the number of bins suggested by rule.
+    """
+    data = np.asarray(data, dtype=np.float_)
+    IQR  = stats.iqr(data, rng=(25, 75), scale=1.0, nan_policy="omit")
+    N    = data.size
+    bw   = (2 * IQR) / np.power(N, 1/3)
+
+    if returnas=="width":
+        result = bw
+    else:
+        datmin, datmax = data.min(), data.max()
+        datrng = datmax - datmin
+        result = int((datrng / bw) + 1)
+    
+    return result
